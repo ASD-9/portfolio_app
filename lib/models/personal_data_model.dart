fd2689b2ddab5be22f0bafb03e12bb29b0be4324
ASD-9 <ryan.chavatte@gmail.com>
2025-08-29 11:56:02 +0200

    base model for data

---
+++ lib/models/personal_data_model.dart
@@ -0,0 +1,28 @@
+class PersonalDataModel {
+  final String name;
+  final String email;
+  final String phone;
+  final String linkedin;
+  final String github;
+  final String about;
+
+  PersonalDataModel({
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.linkedin,
+    required this.github,
+    required this.about
+  });
+
+  factory PersonalDataModel.fromJson(Map<String, dynamic> json) {
+    return PersonalDataModel(
+      name: json['personalInfo']['name'],
+      email: json['personalInfo']["contact"]['email'],
+      phone: json['personalInfo']["contact"]['phone'],
+      linkedin: json['personalInfo']['contact']['linkedin'],
+      github: json['personalInfo']['github'],
+      about: json['personalInfo']['about']
+    );
+  }
+}
